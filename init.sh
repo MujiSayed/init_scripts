4e090112cae355121531129759b3773e20abd39a
Muji Sayed <muji.sayed@gmail.com>
2022-07-23 16:22:42 +0000

    added additional files

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # A Simple bash script for getting servers up and running with k3s and zsh
 
+CURL='/usr/bin/curl'
+ZSHRC='https://raw.githubusercontent.com/MujiSayed/init_scripts/main/.zshrc'
 echo "Updating Repo's"
 sudo apt update -y
 echo "Upgrading packages"
@@ -8,3 +10,6 @@ sudo apt upgrade -y
 
 echo "Installing zsh"
 sudo apt install zsh -y
+
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
